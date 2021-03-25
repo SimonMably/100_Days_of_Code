387cb800673157f684159e0eaae88c36f2b31e5f
SimonMably <simonmably@gmail.com>
2021-03-25 19:48:30 +0000

    Completed and added files for day 28.

---
+++ day_28/dynamic_typing.py
@@ -0,0 +1,24 @@
+
+
+# Strongly Typed:
+# '3 + "4"' cannot be done because Python is a strongly-typed language. This
+#  means that Python will not allow each data type to work with other data
+#  types as they are incompatible. Also, certain methods and functions will
+#  only work on 1 (or so) data type and not others.
+
+# Dynamically Typed:
+# This means that we don't need to declare the data type of variables (as
+# Python will know the ata type of all variables in a program at run-time).
+# Also, we can change the contents of variables whenever we need or want to.
+# We can even change data types.
+
+# Example:
+# Storing an integer in a variable
+a = 3
+print(a)
+
+# Changing the same variables contents to a string
+a = "Hello"
+print(a)
+
+
