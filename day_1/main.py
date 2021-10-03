8f704d6d43973d3ce20f5f73dbb27e186fca180f
SimonMably <simonmably@gmail.com>
2021-10-03 18:09:50 +0100

    Completed and added portfolio project for day 81. Added project for day 1.

---
+++ day_1/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is a name of a pet?\n")
+
+print("Your bands name could be: " + city + " " + pet)
