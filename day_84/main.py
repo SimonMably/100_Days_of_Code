7e8541b263c8b876765f7a0a7871e3340dc5e297
SimonMably <simonmably@gmail.com>
2021-12-03 21:32:17 +0000

    Updated Watermark app: completed function to save watermarked image. Updated other functions to add watermark to images, update staticbitmap to show watermarked images (jpegs only, issue with png images), added try excepts for encountered exceptions.

--- day_84/main.py
+++ day_84/main.py
@@ -1,144 +1,174 @@
-from PIL import Image, ImageFont, ImageDraw
 import wx
+from PIL import Image
 
 
-class WatermarkFrame(wx.Frame):
-    """"""
+class WatermarkApp(wx.Frame):
+
     def __init__(self):
-        super().__init__(parent=None, title="Watermark!")
+        super().__init__(parent=None, title="Watermark")
 
         self.panel = wx.Panel(self)
+        self.max_img_size = 600
 
-        self.PhotoMaxSize = 600
+        # Image Widget - for viewing images
+        img = wx.Image(600, 375)
+        img_control_flags = wx.ID_ANY | wx.FD_FILE_MUST_EXIST
+        self.img_control = wx.StaticBitmap(
+            self.panel, img_control_flags,
+            wx.Bitmap(img)
+        )
 
-        self.create_widgets()
+        # Browse Image Button
+        self.browse_button = wx.Button(self.panel, label="Browse Images")
+        self.browse_button.Bind(wx.EVT_BUTTON, self.browse_images)
+
+        # Add Watermark Button
+        self.add_watermark_button = wx.Button(self.panel, label="Add Watermark")
+        self.add_watermark_button.Bind(wx.EVT_BUTTON, self.add_watermark)
+
+        # Save as New Image Button
+        self.save_button = wx.Button(self.panel, label="Save as New Image")
+        self.save_button.Bind(wx.EVT_BUTTON, self.save_watermarked_image)
+
+        # Sizers / Placing widgets on sizers
+        self.vertical_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.horizontal_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.horizontal_sizer.Add(self.browse_button, 0, wx.ALL, 5)
+        self.horizontal_sizer.Add(self.add_watermark_button, 0, wx.ALL, 5)
+        self.horizontal_sizer.Add(self.save_button, 0, wx.ALL, 5)
+        self.vertical_sizer.Add(self.horizontal_sizer, 0, wx.ALL | wx.CENTRE, 5)
+        self.vertical_sizer.Add(self.img_control, 0, wx.ALL, 5)
+
+        # Setting Main Sizer / Fitting Sizers onto Frame
+        self.panel.SetSizer(self.vertical_sizer)
+        self.vertical_sizer.Fit(self)
+
+        # Showing Widgets
         self.Show()
-        # self.Maximize(True)
-    
-    def create_widgets(self):
-        """Creates all of the widgets to be used and arranges them on the Frame."""
-        instructions = "Browse for an image"
-        
-        img = wx.Image(600, 600)
-        self.image_control = wx.StaticBitmap(self.panel, 
-                                             wx.ID_ANY | wx.FD_FILE_MUST_EXIST | wx.FD_SHOW_HIDDEN, 
-                                             wx.Bitmap(img))
-
-        instructions_label = wx.StaticText(self.panel, label=instructions)
-        self.photo_text = wx.TextCtrl(self.panel, size=(200, -1))
-        
-        browse_button = wx.Button(self.panel, label="Browse Images")
-        browse_button.Bind(wx.EVT_BUTTON, self.browse_images)
-        
-        add_watermark_button = wx.Button(self.panel, label="Add Watermark")
-        add_watermark_button.Bind(wx.EVT_BUTTON, self.add_watermark)
-        
-        save_image_button = wx.Button(self.panel, label="Save As New Image")
-        save_image_button.Bind(wx.EVT_BUTTON, self.save_watermarked_image)
-
-        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        # self.main_sizer.Add(wx.StaticLine(self.panel, wx.ID_ANY), 0,
-        #                     wx.ALL | wx.EXPAND, 5)
-        self.main_sizer.Add(instructions_label, 0, wx.ALL, 5)
-        self.main_sizer.Add(self.image_control, 0, wx.ALL, 5)
-        self.sizer.Add(self.photo_text, 0, wx.ALL, 5)
-        self.sizer.Add(browse_button, 0, wx.ALL, 5)
-        self.sizer.Add(add_watermark_button, 0, wx.ALL, 5)
-        self.sizer.Add(save_image_button, 0, wx.ALL, 5)
-        self.main_sizer.Add(self.sizer, 0, wx.ALL, 5)
-
-        self.panel.SetSizer(self.main_sizer)
-        self.main_sizer.Fit(self)
 
     def browse_images(self, *args):
-        """"""
-        wildcard = "JPEG files (*.jpg)|*.jpg|" \
-                   "PNG files (*.png)|*.png|" \
-                   "BMP files (*.bmp)|*.bmp"
+        """
+        Opens FileDialog box, user to select image to watermark. jpg,
+        png and bmp file formats only.
+        """
+        open_wildcard = "JPEG files (*.jpg)|*.jpg|" \
+                        "PNG files (*.png)|*.png"
 
-        with wx.FileDialog(self, "Select a file to watermark",
-                           wildcard=wildcard, style=wx.FD_OPEN) as open_dialog:
+        with wx.FileDialog(
+                self, "Select a file to watermark",
+                wildcard=open_wildcard, style=wx.FD_OPEN
+        ) as open_dialog:
             if open_dialog.ShowModal() == wx.ID_OK:
-                self.photo_text.SetValue(open_dialog.GetPath())
+                self.path = open_dialog.GetPath()
+                self.view_image(self.path)
             else:
                 return
 
-        self.view_image()
+    def view_image(self, img, *args):
+        """
+        Called in self.browse_images(). Shows image in self.image_control.
+        """
+        # If Image is watermarked image or image opened from FileDialog
+        if isinstance(img, wx.Bitmap):
+            img = wx.Bitmap.ConvertToImage(img)
 
-    def view_image(self, img=None):
-        """"""
-        if img is None:
-            filepath = self.photo_text.GetValue()
-            img = wx.Image(filepath, wx.BITMAP_TYPE_ANY)
+        else:
+            img = wx.Image(img, wx.BITMAP_TYPE_ANY)
 
-        # Scale the image, preserving the aspect ratio
+        # Scaling the image, preserving the aspect ratio
         width = img.GetWidth()
         height = img.GetHeight()
         if width > height:
-            new_width = self.PhotoMaxSize
-            new_height = int(self.PhotoMaxSize * height / width)
+            new_width = self.max_img_size
+            new_height = int(self.max_img_size * height / width)
         else:
-            new_width = self.PhotoMaxSize
-            new_height = int(self.PhotoMaxSize * height / width)
+            new_width = self.max_img_size
+            new_height = int(self.max_img_size * height / width)
         img = img.Scale(new_width, new_height)
 
-        self.image_control.SetBitmap(wx.Bitmap(img))
+        self.img_control.SetBitmap(wx.Bitmap(img))
         self.panel.Refresh()
-    
+
     def add_watermark(self, *args):
-        """"""
+        """
+        Uses Python Image Library (PIL / pillow) to place a watermark on a user
+        selected image.
+        """
         try:
-            img = Image.open(self.photo_text.GetValue())
+            if self.path.endswith(".png"):
+                img = Image.open(self.path).convert("RGBA")
+            else:
+                img = Image.open(self.path).convert("RGB")
+
+            self.copied_img = img.copy()  # .convert("RGBA")
+
+            # For PNG images, setting mask to white (regarding background
+            # transparency)
+            self.logo = Image.open("sm_logo.png").convert("RGBA")
+            logo_size = (20, 20)
+            self.logo.resize(logo_size)
+
+            watermark_position = (self.copied_img.size[0] - 100,
+                                  self.copied_img.size[1] - 130)
+            self.copied_img.paste(self.logo, watermark_position, self.logo)
+
+            self.watermarked_img = self._pil_to_wx(self.copied_img)
+
         except AttributeError:
-            error_message = "You must open an image before you can use this feature."
-            styles = wx.OK | wx.CENTRE | wx.ICON_ERROR
-            error_message_box = wx.MessageBox(error_message, caption="Error",
-                                              style=styles)
+            message = "You need to open an image before then watermark " \
+                      "feature can be used"
+            styles = wx.OK | wx.ICON_ERROR
+            no_image_selected = wx.MessageBox(
+                message=message, caption="Warning",
+                style=styles
+            )
         else:
-            # if no errors occurred
-            print(img.format, img.size, img.mode)
-
-            # A copy of the above image
-            self.watermarked_img = img.copy()
-
-            draw = ImageDraw.Draw(self.watermarked_img)
-            # TODO: Below works. Work on coordinate positioning, watermark,
-            #                    font style/size,
-            draw.text((img.size[0] - 50, img.size[1] - 50), "hello", font=None)
-
-            # FIX THIS: Causes -> TypeError: Expected a list of bytes objects.
-            self.watermarked_img = wx.Bitmap.ConvertToImage(
-                wx.Bitmap(self.watermarked_img))
-
             self.view_image(self.watermarked_img)
-
-            # self.image_control.SetBitmap(wx.Bitmap(self.watermarked_img))
-            # self.panel.Refresh()
-
-            # self.view_image()  # TODO: get this to work with an unsaved
-            #                            watermarked image, if possible with
-            #                            wxPython. If not, use .show()
-            #self.watermarked_img.save("test_img.jpg")
+            self.panel.Refresh()
 
     def save_watermarked_image(self, *args):
-        """"""
-        # TODO: Find a way to use wx.FileDialog() to save image after it has been watermarked
-        # IDEA:
-        to_save_message = "Would you like to save as a new image?"
-        styles = wx.YES_NO | wx.CENTRE | wx.ICON_QUESTION
-        to_save = wx.MessageDialog(self, to_save_message, caption="Save",
-                                   style=styles)
-        result = to_save.ShowModal()
+        """
+        After image has been watermarked, user can save image to a selected
+        directory
+        """
+        try:
+            save_wildcard = "All files (*.*)|*.*"
+            style = wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT
 
-        if result == wx.ID_YES:
-            print(result)
-        else:
-            print(result)
+            # image = self._wx_to_pil(self.watermarked_img)
+            image = self.copied_img
+
+            with wx.FileDialog(
+                    self, "Save image:", ".", "", wildcard=save_wildcard,
+                    style=style
+            ) as save_dialog:
+                if save_dialog.ShowModal() == wx.ID_OK:
+                    filename = save_dialog.GetFilename()
+                    # directory_name = save_dialog.GetDirectory()
+                    image.save(filename)
+                else:
+                    return
+        except AttributeError:
+            message = "You need to open an image before you can save an image"
+            styles = wx.OK | wx.ICON_ERROR
+            no_image_selected = wx.MessageBox(
+                message=message, caption="Warning", style=styles
+            )
+        except ValueError:
+            message = "Incorrect File Extension. Please make sure you enter " \
+                      "the correct file extension."
+            styles = wx.OK | wx.ICON_ERROR
+            no_image_selected = wx.MessageBox(
+                message=message, caption="Warning", style=styles
+            )
+
+    def _pil_to_wx(self, image):
+        """Helper method. Convert PIL Image() into wx.Bitmap()."""
+        width, height = image.size
+        return wx.Bitmap.FromBuffer(width, height, image.tobytes())
 
 
 if __name__ == "__main__":
     app = wx.App()
-    frame = WatermarkFrame()
+    frame = WatermarkApp()
     app.MainLoop()
