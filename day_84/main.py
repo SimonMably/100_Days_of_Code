4f05892bc1c90c180cb8c446da7557961996e15d
SimonMably <simonmably@gmail.com>
2021-12-05 14:49:52 +0000

    Completed Portfolio Project for day 84. Fixed _pil_to_wx_image() function (pil image to wxPython bitmap image conversion), function now in working order.

--- day_84/main.py
+++ day_84/main.py
@@ -3,6 +3,10 @@ from PIL import Image
 
 
 class WatermarkApp(wx.Frame):
+    """
+    A watermarking app made with wxPython and Pillow (Python Imaging Library.
+    This app can place a png logo onto png and jpeg images.
+    """
 
     def __init__(self):
         super().__init__(parent=None, title="Watermark")
@@ -86,7 +90,7 @@ class WatermarkApp(wx.Frame):
             new_height = int(self.max_img_size * height / width)
         img = img.Scale(new_width, new_height)
 
-        self.img_control.SetBitmap(wx.Bitmap(img))
+        self.img_control.SetBitmap(wx.Bitmap(img))  #.SetMask("RGBA"))
         self.panel.Refresh()
 
     def add_watermark(self, *args):
@@ -104,19 +108,29 @@ class WatermarkApp(wx.Frame):
 
             # For PNG images, setting mask to white (regarding background
             # transparency)
-            self.logo = Image.open("sm_logo.png").convert("RGBA")
-            logo_size = (20, 20)
-            self.logo.resize(logo_size)
+            try:
+                # In case specified logo is not in directory
+                self.logo = Image.open("sm_logo.png").convert("RGBA")
+            except FileNotFoundError:
+                message = "Your logo image isn't where it should be"
+                styles = wx.OK | wx.ICON_ERROR
+                no_logo_selected = wx.MessageBox(
+                    message=message, caption="Warning",
+                    style=styles
+                )
+                return
+            else:
+                logo_size = (20, 20)
+                self.logo.resize(logo_size)
 
-            watermark_position = (self.copied_img.size[0] - 100,
-                                  self.copied_img.size[1] - 130)
-            self.copied_img.paste(self.logo, watermark_position, self.logo)
+                watermark_position = (self.copied_img.size[0] - 100,
+                                      self.copied_img.size[1] - 130)
+                self.copied_img.paste(self.logo, watermark_position, self.logo)
 
-            self.watermarked_img = self._pil_to_wx(self.copied_img)
-
+                self.watermarked_img = self._pil_to_wx_img(self.copied_img)
         except AttributeError:
             message = "You need to open an image before then watermark " \
-                      "feature can be used"
+                     "feature can be used"
             styles = wx.OK | wx.ICON_ERROR
             no_image_selected = wx.MessageBox(
                 message=message, caption="Warning",
@@ -144,7 +158,6 @@ class WatermarkApp(wx.Frame):
             ) as save_dialog:
                 if save_dialog.ShowModal() == wx.ID_OK:
                     filename = save_dialog.GetFilename()
-                    # directory_name = save_dialog.GetDirectory()
                     image.save(filename)
                 else:
                     return
@@ -162,10 +175,28 @@ class WatermarkApp(wx.Frame):
                 message=message, caption="Warning", style=styles
             )
 
-    def _pil_to_wx(self, image):
-        """Helper method. Convert PIL Image() into wx.Bitmap()."""
-        width, height = image.size
-        return wx.Bitmap.FromBuffer(width, height, image.tobytes())
+    def _pil_to_wx_img(self, image, copy_alpha=True):
+        """Convert PIL Image() into wx.Bitmap()."""
+        has_alpha = image.mode[-1] == "A"
+
+        # Make sure there is an alpha layer
+        if copy_alpha and has_alpha:
+            wx_image = wx.Image(image.size)
+            pil_img_copy_rgba = image.copy()
+            pil_img_copy_rgb = pil_img_copy_rgba.convert("RGB")  # RGBA -> RGB
+            pil_img_rgb_data = pil_img_copy_rgb.tobytes()
+            wx_image.SetData(pil_img_rgb_data)
+            wx_image.SetAlpha(pil_img_copy_rgba.tobytes()[3::4])
+        else:
+            wx_image = wx.Image(image.size)
+            pil_img_copy = image.copy()
+
+            # Discard any alpha from PIL image
+            pil_img_copy_rgb = pil_img_copy.convert("RGB")
+            pil_img_rgb_data = pil_img_copy_rgb.tobytes()
+            wx_image.SetData(pil_img_rgb_data)
+
+        return wx_image.ConvertToBitmap()
 
 
 if __name__ == "__main__":
